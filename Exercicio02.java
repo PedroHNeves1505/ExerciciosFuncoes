7c03fd71595f88832a74dc6cdb969e8c6ee28237
Pedro Henrique Neves <158407213+PedroHNeves1505@users.noreply.github.com>
2025-05-21 16:49:46 -0300

    Exercicio02.java

---
+++ Exercicio02.java
@@ -0,0 +1,12 @@
+public class Exercicio02 {
+    
+    public static void main(String[] args) {
+        imprimeNovaMensagem("oi", "olá", "hello");
+    }
+
+    public static void imprimeNovaMensagem(String parametro1, String parametro2, String parametro3){
+        System.out.println("Meu primeiro programa com funções e parametro: " + parametro1);
+        System.out.println("Meu primeiro programa com funções e parametro: " + parametro2);
+        System.out.println("Meu primeiro programa com funções e parametro: " + parametro3);
+    }
+}
